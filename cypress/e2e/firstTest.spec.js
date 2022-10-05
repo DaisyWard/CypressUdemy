33a84bcc0db5dcda336ab2421e1f42f121ba4f18
DaisyyWard <daisy.w@mmtdigital.co.uk>
2022-10-05 15:56:14 +0100

    Test window dialog boxes

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -298,12 +298,26 @@ describe('Test grabbing an element', () => {
     })
   })
 
-  it.only('tests tooltips', () => {
+  it('tests tooltips', () => {
     cy.visit('/')
     cy.contains('Modal & Overlays').click()
     cy.contains('Tooltip').click()
 
     cy.contains('nb-card', 'Colored Tooltips').contains('Default').click()
     cy.get('nb-tooltip').should('contain', 'This is a tooltip')
+  })
+
+  it.only('tests dialog boxes', () => {
+    cy.visit('/')
+    cy.contains('Tables & Data').click()
+    cy.contains('Smart Table').click()
+
+    const stub = cy.stub()
+    cy.on('window:confirm', stub)
+    cy.get('tbody tr').first().find('.nb-trash').click().then(() => {
+      expect(stub.getCall(0)).to.be.calledWith('Are you sure you want to delete?')
+    })
+
+    //cy.on('window:confirm', () => false) //If you don't want to confirm
   })
 })
